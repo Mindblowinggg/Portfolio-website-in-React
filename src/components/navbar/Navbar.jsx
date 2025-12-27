ea3721403120ebb42c851815ae2371a659a855ef
Mindblowinggg <akahar36@gmail.com>
2025-12-27 11:46:04 +0530

    sd

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useContext } from "react";
+// eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
 import Styles from "./Navbar.module.css";
 import logo from "/A.png";
